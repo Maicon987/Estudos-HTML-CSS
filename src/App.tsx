127ec59a991975340fda5c59ca383f0abce41522
Maicon987 <barbosamaicon67@gmail.com>
2026-06-19 10:45:15 -0300

    Estudos HTML e CSS com listas, cores e espaçamentos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
+    <nav>
+      <ul>
+        <li>Inicio</li>
+        <li>Sobre</li>
+        <li>Contato</li>
+        <li>Mapa</li>
+      </ul>
+    </nav>
+      <p>este é um paragrafo</p>
+      <p className='maior'>este é outro paragrafo</p>
+      <ul>
+      <li>Inicio</li>
+      <li>Sobre</li>
+      <li>Contato</li>
+      <li>Mapa</li>
+    </ul>
+      <button>Botão</button>
       
     </>
   )
